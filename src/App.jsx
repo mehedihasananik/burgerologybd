5a8514fdeb0157aa1b23a6dc71e113783c0128fb
marshallanique <marshallanique@gmail.com>
2023-10-11 13:42:27 +0600

    deleted categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import "./App.css";
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  Outlet,
-} from "react-router-dom";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { Layout, Home } from "../src/components";
-import SingleCategory from "./components/SingleCategory/SingleCategory";
-import SingleFood from "./components/SingleFood/SingleFood";
 
 const router = createBrowserRouter([
   {
@@ -21,19 +12,6 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
-        loader: () => fetch(`http://localhost:5000/foods`),
-      },
-      {
-        path: "/category/:id",
-        element: <SingleCategory />,
-        loader: ({ params }) =>
-          fetch(`http://localhost:5000/categories/${params.id}`),
-      },
-      {
-        path: "/foods/:id",
-        element: <SingleFood />,
-        loader: ({ params }) =>
-          fetch(`http://localhost:5000/foods/${params.id}`),
       },
     ],
   },
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,9 @@
-import React from "react";
 import Hero from "../Hero/Hero";
-import Categories from "../Categories/Categories";
-import { useLoaderData } from "react-router-dom";
-import SingleCategory from "../SingleCategory/SingleCategory";
 
 const Home = () => {
-  const foods = useLoaderData();
-
   return (
     <div>
       <Hero />
-      <Categories />
     </div>
   );
 };
